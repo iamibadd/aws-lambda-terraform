2a85b56fcc6758ef4ebf4ea8deef45a8c3bfdb51
Ibad Shah <iamibadd@gmail.com>
2022-12-01 23:46:26 +0500

    Subject: Lambda function setup.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#export AWS_ACCESS_KEY_ID="AKIAQTHXZ25UNOJ4HOUY"
-#export AWS_SECRET_ACCESS_KEY="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"
 terraform init
 terraform plan -out tfplan.out
 terraform apply tfplan.out
