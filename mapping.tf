2294199b270bbd86a3f10236095a72e1aef59167
Ibad Shah <iamibadd@gmail.com>
2022-12-03 03:18:00 +0500

    Subject: Terraform lambda functions and sqs trigger added.

--- mapping.tf
+++ mapping.tf
@@ -1,5 +1,5 @@
-## Maps hello_world lambda function with SQS Queue
-#resource "aws_lambda_event_source_mapping" "hello_world_sqs_mapping" {
-#  event_source_arn = aws_sqs_queue.hello_world_queue.arn
-#  function_name    = aws_lambda_function.hello_world.arn
-#}
+# Maps hello_world lambda function with SQS Queue
+resource "aws_lambda_event_source_mapping" "hello_world_sqs_mapping" {
+  event_source_arn = aws_sqs_queue.hello_world_queue.arn
+  function_name    = aws_lambda_function.hello_world.arn
+}
--- src/trigger.js
+++ src/trigger.js
@@ -4,44 +4,28 @@ const {
     AWS_REGION,
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY,
-    trigger_function_name
 } = process.env;
+
+console.log(AWS_REGION);
+console.log(AWS_ACCESS_KEY_ID);
+console.log(AWS_SECRET_ACCESS_KEY);
 
 AWS.config.update({region: AWS_REGION, accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY});
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
+
+// (async () => {
 module.exports.handler = async (event) => {
-    const uniqueId = uuid.v4();
-    console.log(trigger_function_name);
-    console.log(AWS_REGION);
-    console.log(AWS_ACCESS_KEY_ID);
-    console.log(AWS_SECRET_ACCESS_KEY);
-    const data = {
-        MessageBody: JSON.stringify({name: 'Ibad Shah', age: 69}),
-        MessageDeduplicationId: uniqueId,
-        MessageGroupId: uniqueId,
-        QueueUrl: "https://sqs.us-east-2.amazonaws.com/041321879400/terraform-example-queue.fifo"
-    };
     for (let i = 0; i < 50; i++) {
-        let sendSqsMessage = await sqs.sendMessage(data).promise();
+        const uniqueId = uuid.v4();
+        const data = {
+            MessageBody: JSON.stringify({name: 'Ibad Shah', age: 69}),
+            MessageDeduplicationId: uniqueId,
+            MessageGroupId: uniqueId,
+            QueueUrl: "https://sqs.us-east-2.amazonaws.com/041321879400/terraform-example-queue.fifo"
+        };
+        const sendSqsMessage = await sqs.sendMessage(data).promise();
         console.log(sendSqsMessage.MessageId);
     }
 }
-
-// (async () => {
-//     const uniqueId = uuid.v4();
-//     console.log(trigger_function_name);
-//     console.log(AWS_REGION);
-//     console.log(AWS_ACCESS_KEY);
-//     console.log(AWS_SECRET_KEY);
-//     const data = {
-//         MessageBody: JSON.stringify({name: 'Ibad Shah', age: 69}),
-//         MessageDeduplicationId: uniqueId,
-//         MessageGroupId: uniqueId,
-//         QueueUrl: "https://sqs.us-east-2.amazonaws.com/041321879400/terraform-example-queue.fifo"
-//     };
-//     for (let i = 0; i < 50; i++) {
-//         const sendSqsMessage = await sqs.sendMessage(data).promise();
-//         console.log(sendSqsMessage.MessageId);
-//     }
 // })()
